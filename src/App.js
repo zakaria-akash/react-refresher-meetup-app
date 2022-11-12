b3f4593d1f56783e28e1ea97948748d0e433fa5c
zakaria-akash <zakaria.93@yahoo.com>
2022-11-13 01:32:20 +0600

    adding layout part with MainNavigation component

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { Route, Switch } from "react-router-dom";
 import AllMeetups from "./pages/AllMeetups";
 import Favorites from "./pages/Favorites";
 import NewMeetup from "./pages/NewMeetup";
+import MainNavigation from "./components/layout/MainNavigation";
 
 const App = () => {
   return (
     <div>
+      <MainNavigation />
       <Switch>
         <Route exact path="/">
           <AllMeetups />
