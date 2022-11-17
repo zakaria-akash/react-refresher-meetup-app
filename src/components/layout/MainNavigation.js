af87dc5e98d0edc2905e992f1fb7a912e418c574
zakaria-akash <zakaria.93@yahoo.com>
2022-11-17 21:47:23 +0600

    showing all favorite item temporarily in the Favorite page by rebdering favorite meetups through MeetupList component; adding a badge to the header to show the total numbers of favorite items

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import FavoritesContext from "../../store/favorites-context";
 
 import classes from "./MainNavigation.module.css";
 
 const MainNavigation = () => {
+  const favContext = useContext(FavoritesContext);
   return (
     <header className={classes.header}>
       <div className={classes.logo}>React Meetups</div>
@@ -16,7 +18,10 @@ const MainNavigation = () => {
             <Link to="/new-meetup">Add New Meetup</Link>
           </li>
           <li>
-            <Link to="/favorites">My Favorite Meetups</Link>
+            <Link to="/favorites">
+              My Favorite Meetups
+              <span className={classes.badge}>{favContext.totalFavorites}</span>
+            </Link>
           </li>
         </ul>
       </nav>
--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
+import FavoritesContext from "../store/favorites-context";
+import MeetupList from "../components/meetups/MeetupList";
 
 const Favorites = () => {
-  return (
-    <div>
-      <h1>Favorites Page</h1>
-    </div>
-  );
+  const favContext = useContext(FavoritesContext);
+  if (favContext.totalFavorites === 0) {
+    return (
+      <section>
+        <h3>
+          No meetup has been selected yet as{" "}
+          <strong>
+            <u>Favorite</u>!
+          </strong>
+        </h3>
+      </section>
+    );
+  } else {
+    return (
+      <section>
+        <h1>My Favorite Meetups</h1>
+        <MeetupList meetups={favContext.favorites} />
+      </section>
+    );
+  }
 };
 
 export default Favorites;
